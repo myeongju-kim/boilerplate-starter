47313d86104a636c95d5cd57d9be5ab2c175cf22
myeongju-kim <kingmj@daou.co.kr>
2025-09-23 11:21:02 +0900

    build: configure build.gradle for Gradle plugin publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,51 @@
 plugins {
-    id("java")
+    id("java-gradle-plugin")
+    id("com.gradle.plugin-publish") version "1.2.1"
+    kotlin("jvm") version "1.9.24"
+    id("maven-publish")
 }
 
 group = "org.codegen"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
+java {
+    toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
+    withSourcesJar()
+    withJavadocJar()
+}
+
 dependencies {
+    implementation("org.freemarker:freemarker:2.3.32")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.freemarker:freemarker:2.3.32")
-    runtimeOnly("com.mysql:mysql-connector-j:8.3.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+gradlePlugin {
+    plugins {
+        create("codegenPlugin") {
+            id = "org.codegen"
+            implementationClass = "org.codegen.CodegenPlugin"
+            displayName = "DB Codegen Plugin"
+            description = "Generates boilerplate code from DB schema at build time"
+            website.set("https://github.com/myeongju-kim/boilerplate-starter")
+            vcsUrl.set("https://github.com/myeongju-kim/boilerplate-starter.git")
+            tags.set(listOf("codegen", "db", "generator", "spring"))
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
 }
