0acfb41fd14a65a23e2e2147b818c01818230278
myeongju-kim <kingmj@daou.co.kr>
2025-09-23 15:23:13 +0900

    feat: add projectRoot property to CodegenConfig

--- src/main/java/org/codegen/config/CodegenConfig.java
+++ src/main/java/org/codegen/config/CodegenConfig.java
@@ -5,11 +5,13 @@ import java.nio.file.Paths;
 
 public class CodegenConfig {
     private final String basePackage; // 예: "kr.co.daou.app"
+    private final Path projectRoot; // 현재 프로젝트 root
     private final Path outRoot; // 예: Paths.get("generated-src")
 
     public CodegenConfig(String basePackage, String outRoot) {
         this.basePackage = basePackage;
-        this.outRoot = Paths.get(outRoot);
+        this.projectRoot = Paths.get(System.getProperty("user.dir"));
+        this.outRoot = projectRoot.resolve(outRoot);
     }
 
     public String getBasePackage() { return basePackage; }
