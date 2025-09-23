7d1dee8a6a7df4ebefee0e1116cfc3993382eaf7
myeongju-kim <kingmj@daou.co.kr>
2025-09-23 15:24:21 +0900

    fix: change cli mode name

---
+++ src/main/java/org/codegen/CodegenCli.java
@@ -0,0 +1,14 @@
+package org.codegen;
+
+import org.codegen.generator.CodegenExecutor;
+import org.codegen.generator.CodegenInputs;
+import org.codegen.utils.ArgsParser;
+
+public class CodegenCli {
+
+    public static void main(String[] args) throws Exception {
+
+        CodegenInputs in = ArgsParser.parse(args);
+        new CodegenExecutor().run(in);
+    }
+}
